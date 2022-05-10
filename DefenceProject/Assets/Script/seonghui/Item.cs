3c665f6d3bc97d961a67c863dad689ace4350091
Seonghui <dhqks507@gmail.com>
2022-05-11 04:40:01 +0900

    [Item]
    Item Script 작성
    Timer, 배터리 잔량 표기

---
+++ DefenceProject/Assets/Script/seonghui/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "New Item/item")]
+public class Item : ScriptableObject
+{
+    public enum ItemType
+    {
+        Battery,
+        Clip,
+        Gun,
+        Bullet
+    }
+    public string itemName;
+    public ItemType itemType;
+    public Sprite itemImage;
+    public GameObject itemPrefab;
+}
