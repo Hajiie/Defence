06452382f131a4454e65f08b28c7b571496b6829
doyeonk429 <doyeonk429@gmail.com>
2022-05-13 19:54:35 +0900

    [Light] Setting Basic instances
    - Add LightWeight2d Asset and PipeLine etc
    - Add PointLight

---
+++ Defence/Assets/Scripts/DY/DetectObj.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectObj : MonoBehaviour
+{
+    RaycastHit2D hit;
+    Vector3 raycastDir = new Vector3(0, 0, 1);
+    Vector3 mousePos;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            CheckConflict();
+        }
+    }
+    void CheckConflict()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(1, 1, 1);
+        Debug.Log(mousePos);
+        Debug.DrawRay(mousePos, new Vector3(0, 0, 10), Color.red, 0.5f); 
+        hit = Physics2D.Raycast(mousePos, raycastDir, 15f);
+
+        if(hit)
+        {
+            Debug.Log(hit.collider.name);
+        }
+    }
+}
---
+++ Defence/Assets/Scripts/DY/MoveToClick.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToClick : MonoBehaviour
+{
+    RaycastHit2D hit;
+    Vector3 raycastDir = new Vector3(0, 0, 1);
+    Vector3 mousePos, transPos, targetPos;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            CallTargetPos();
+        }
+        MoveToTargetandCheck();
+    }
+    void CallTargetPos()
+    {
+        mousePos = Input.mousePosition;
+        transPos = Camera.main.ScreenToWorldPoint(mousePos + new Vector3(10, 10, 10));
+        Debug.Log(transPos);
+        targetPos = new Vector3((transPos.x+0.1f), (transPos.y+0.1f), 0);
+        
+        Debug.DrawRay(transform.position, new Vector3(0, 0, 10), Color.red, 1f);
+        hit = Physics2D.Raycast(transform.position, raycastDir, 5f);
+
+        if(hit)
+        {
+            Debug.Log(hit.collider.name);
+        }
+    }
+    void MoveToTargetandCheck()
+    {
+        transform.position = targetPos;
+    }
+}
