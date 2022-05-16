645571986b76495fe4827dedcc8a1a801d8fee18
doyeonk429 <doyeonk429@gmail.com>
2022-05-16 17:07:54 +0900

    [Light] Set Btn
    - object tag
    - add light onoff btn
    - show object tag
    - show object btn down message

---
+++ Defence/Assets/Scripts/DY/LightOnOff.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOnOff : MonoBehaviour
+{
+    public GameObject pointLight;
+    public bool lightOn = false;
+
+    public void SetLight()
+    {
+        if(lightOn)
+        {
+            pointLight.SetActive(false);
+            lightOn = false;
+        }
+        else{
+            pointLight.SetActive(true);
+            lightOn = true;
+        }
+    }
+    public void ShowTempMessage()
+    {
+        Debug.Log("버튼이 눌렸습니다. 아직 만드는 중..");
+    }
+}
--- Defence/Assets/Scripts/DY/MoveToClick.cs
+++ Defence/Assets/Scripts/DY/MoveToClick.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveToClick : MonoBehaviour
 {
     RaycastHit2D hit;
     Vector3 raycastDir = new Vector3(0, 0, 1);
     Vector3 mousePos, transPos, targetPos;
+    public GameObject cabinetBtn, closetBtn;
 
     void Start()
     {
@@ -20,25 +22,44 @@ public class MoveToClick : MonoBehaviour
         {
             CallTargetPos();
         }
-        MoveToTargetandCheck();
     }
     void CallTargetPos()
     {
         mousePos = Input.mousePosition;
         transPos = Camera.main.ScreenToWorldPoint(mousePos + new Vector3(10, 10, 10));
-        Debug.Log(transPos);
-        targetPos = new Vector3((transPos.x+0.1f), (transPos.y+0.1f), 0);
-        
-        Debug.DrawRay(transform.position, new Vector3(0, 0, 10), Color.red, 1f);
+        //Debug.Log(transPos);
+        targetPos = new Vector3((transPos.x+0.001f), (transPos.y+0.001f), 0);
+        transform.position = targetPos;
+        //Debug.DrawRay(transform.position, new Vector3(0, 0, 10), Color.red, 1f);
         hit = Physics2D.Raycast(transform.position, raycastDir, 5f);
 
         if(hit)
         {
-            Debug.Log(hit.collider.name);
+            Debug.Log(hit.collider.tag);
+            ShowBtn();
+        }
+        else{
+            Debug.Log("아무것도 눌러지지 않음");
+            HideAllBtn();
         }
     }
-    void MoveToTargetandCheck()
+
+    void ShowBtn()
     {
-        transform.position = targetPos;
+        cabinetBtn.SetActive(false);
+        closetBtn.SetActive(false);
+        if(hit.collider.tag == "Cabinet")
+        {
+            cabinetBtn.SetActive(true);
+        }
+        else if(hit.collider.tag == "Closet")
+        {
+            closetBtn.SetActive(true);
+        }
+    }
+    void HideAllBtn()
+    {
+        cabinetBtn.SetActive(false);
+        closetBtn.SetActive(false);
     }
 }
