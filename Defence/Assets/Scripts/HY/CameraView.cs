d9874694401b160d194d3c4c8b543bbd2787d943
Hayoung <104951924+Hayoungaaaa@users.noreply.github.com>
2022-05-27 17:05:53 +0900

    [Camera] 클로징 구현

--- Defence/Assets/Scripts/HY/CameraView.cs
+++ Defence/Assets/Scripts/HY/CameraView.cs
@@ -15,23 +15,29 @@ public class CameraView : MonoBehaviour
     }
 
     public int currentImgLocation;
-    public GameObject downArrow;
-    public GameObject upArrow;
     public GameObject updownArrow;
+    public GameObject upArrow;
+    public GameObject downArrow;
     public GameObject leftrightArrow;
+
+    //int [] YesLeftRight = {(int)CameraLocation.BG_Lock, (int)CameraLocation.BG_Bed,(int)CameraLocation.BG_Lamp,(int)CameraLocation.BG_Door};
+    //int[] noDown = { 4, 7, 10 };
 
     public enum CameraLocation // 옮겨진 카메라 위치
     {
         BG_Lock = 0, // 처음 화면
         BG_Lock_Drawer = 1, // 처음 화면 - 서랍
         BG_Lock_Closet = 2, // 처음 화면 - 옷장
+        
         BG_Bed = 3, // 침실   
         BG_UndertheBed = 4, // 침대 아래
         BG_Lamp = 5, // 램프 있는 방
         BG_Lamp_ToyBox = 6, // 장난감 상자
+
         BG_Lamp_Lamp = 7, // 램프
         BG_Lamp_Drawer = 8, // 서랍
         BG_Lamp_Window = 9, // 창문
+
         BG_Door = 10, // 문 있는 방
         BG_Door_Door = 11, // 문 있는 방
     }
@@ -51,6 +57,7 @@ public class CameraView : MonoBehaviour
             Cameras[i].transform.position = ImageLocation;
             // 카메라 위치 옮겨주기 - 번호에 해당하는 이미지들의 위치 + z -10
         }
+        // start문에서 다 꺼주고 시작해야되나?
     }
 
     void Update()
@@ -59,12 +66,16 @@ public class CameraView : MonoBehaviour
         {
             if (Cameras[i].gameObject.activeSelf == true) // 해당 카메라가 켜져있으면
             {
-                currentImgLocation = i; // 해당 카메라 위치 = 현재 위치
-                //UpDdownArrowOn();
+                currentImgLocation = i; // 해당 카메라 위치 = 현재 위치  
+                //ArrowOnOff();
             }
-        }
-    }
 
+            // 현재 카메라 위치(i)가 배열에 포함되어 있으면(조건 보고)
+        }
+        // 왼쪽 오른쪽은 0,3,7,10 일 때만 켜주기
+        // 위쪽은 4일 때만
+        // 아래쪽은 1,2,3,5,6,8,9,11일 때만 켜주기
+    }
 
     public void LeftArrow() // 왼쪽 이동버튼 눌렀을 때
     {
@@ -84,11 +95,6 @@ public class CameraView : MonoBehaviour
         {
             NextCameraOn((int)CameraLocation.BG_Lock);
         }
-        else
-        {
-            downArrow.SetActive(false);
-        }
-
         // 나머지 화면인 경우 Setactive(false)
     }
 
@@ -114,38 +120,38 @@ public class CameraView : MonoBehaviour
         // 나머지 화면인 경우 Setactive(false)
     }
 
-    public void UpArrow() // 위 버튼을 눌렀을 때
+    public void UpArrow() // 위 이동 버튼 눌렀을 때
     {
-        if (currentImgLocation == ((int)CameraLocation.BG_UndertheBed)) // 현재 위치가 침대아래일 때
+        if (currentImgLocation == ((int)CameraLocation.BG_UndertheBed))
+        // BG_Bed로 되돌아가기
         {
-            NextCameraOn((int)CameraLocation.BG_Bed); // 침대 이동하고;
+            NextCameraOn((int)CameraLocation.BG_Bed);
         }
     }
 
-    public void DownArrow()
+    public void DownArrow() // 아래 이동 버튼 눌렀을 때
     {
         if (currentImgLocation == ((int)CameraLocation.BG_Bed)) // 현재 위치가 침대일 때
         {
             NextCameraOn((int)CameraLocation.BG_UndertheBed);
         }
-    }
+        else if(currentImgLocation == ((int)CameraLocation.BG_Lock_Drawer)|| currentImgLocation == ((int)CameraLocation.BG_Lock_Closet))
+        // BG_Lock으로 되돌아가기
+        {
+            NextCameraOn((int)CameraLocation.BG_Lock);
+        }
+        else if (currentImgLocation == ((int)CameraLocation.BG_Lamp_ToyBox) || currentImgLocation == ((int)CameraLocation.BG_Lamp_Lamp) || currentImgLocation == ((int)CameraLocation.BG_Lamp_Drawer) || currentImgLocation == ((int)CameraLocation.BG_Lamp_Window))
+        // BG_Lamp으로 되돌아가기
+        {
+            NextCameraOn((int)CameraLocation.BG_Lamp);
+        }
+        else if (currentImgLocation == ((int)CameraLocation.BG_Door_Door))
+        // BG_Door로 되돌아가기
+        {
+            NextCameraOn((int)CameraLocation.BG_Door);
+        }
 
-    /*public void UpDdownArrowOn() // 위아래 화살표 켜고 꺼주기
-    {
-        if (currentImgLocation == ((int)CameraLocation.BG_Bed))
-        {
-            downArrow.SetActive(true); // BG_Bed일 때 켜기
-        }
-        else if (currentImgLocation == ((int)CameraLocation.BG_UndertheBed))
-        {
-            upArrow.SetActive(true); // BG_Bed일 때 켜기 // 아 안꺼주는 구나.. // if문 어따 붙여야 되지
-        }
-        else
-        {
-            updownArrow.SetActive(false);
-        }
     }
-    */
 
     public void NextCameraOn(int nextcamera) // 입력받은 번호의 카메라 켜주기
     {
@@ -158,6 +164,32 @@ public class CameraView : MonoBehaviour
                 Cameras[i].gameObject.SetActive(false);
             }
         }
+    }  
+
+    public void ArrowOnOff()
+    {
+        if (!(currentImgLocation == (int)CameraLocation.BG_Lock|| currentImgLocation == (int)CameraLocation.BG_Bed || currentImgLocation == (int)CameraLocation.BG_Lamp || currentImgLocation == (int)CameraLocation.BG_Door))
+        // 원래 켜져 있는데, 메인 4개 + 침대 밑 아니면 다 꺼줘
+        // 메인 4개가 아니면?
+        {
+            leftrightArrow.SetActive(false);// 왼쪽 오른쪽 이동 꺼주기
+            downArrow.SetActive(true); // 이렇게 하면 침대 밑도 켜지겠지
+        }
+        else if (!(currentImgLocation == (int)CameraLocation.BG_UndertheBed))
+            // 원래 켜져있는데, 침대 밑 제외하고 다 꺼주기
+            // 침대 밑이 아니면?
+            // 껐으면 다시 켜야되는데,, 
+        {
+            upArrow.SetActive(false); // 위에 버튼 켜주기
+        }
+ 
+
+        //else if()
+        // 현재 위치가 침대 아래가 아니면 꺼주기
+                
+               
+            // i가 배열에 들어있으면
+       
     }
 }
 
---
+++ Defence/Assets/Scripts/HY/ExpansionBtn.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpansionBtn: MonoBehaviour
+{
+    [SerializeField] List<GameObject> btns = new List<GameObject>(); // 확대 버튼들
+
+    public CameraView cameraview;
+
+    public void Expand() // 버튼이 켜져있는데 눌렀을 경우 이동 시키기 : 무슨 버튼일 경우? 
+    {
+
+        for (int i = 0; i < btns.Count; i++) // 버튼 중에서 돌리고
+        {
+            if (btns[i].activeSelf) // 해당 확대 버튼이 active면
+            {
+                //Debug.Log(i);
+                switch (i) // 버튼 번호에 따라
+                {
+                    case 0:
+                        cameraview.NextCameraOn((int)CameraView.CameraLocation.BG_Lock_Closet);
+                        break;
+                    case 1:
+                        cameraview.NextCameraOn((int)CameraView.CameraLocation.BG_Lock_Drawer);
+                        break;
+                    case 2:
+                        cameraview.NextCameraOn((int)CameraView.CameraLocation.BG_Lamp_ToyBox);
+                        break;
+                    case 3:
+                        cameraview.NextCameraOn((int)CameraView.CameraLocation.BG_Lamp_Lamp);
+                        break;
+                    case 4:
+                        cameraview.NextCameraOn((int)CameraView.CameraLocation.BG_Lamp_Drawer);
+                        break;
+                    case 5:
+                        cameraview.NextCameraOn((int)CameraView.CameraLocation.BG_Lamp_Window);
+                        break;
+                    case 6:
+                        cameraview.NextCameraOn((int)CameraView.CameraLocation.BG_Door_Door);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+    
+}
