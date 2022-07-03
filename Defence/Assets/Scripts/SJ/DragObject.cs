01b28e1e8c2398139227dd277ca21fccb9e462b2
water-bell <wwkk548@gmail.com>
2022-07-04 04:26:25 +0900

    [Stage3] DragObject

--- Defence/Assets/Scripts/SJ/DragObject.cs
+++ Defence/Assets/Scripts/SJ/DragObject.cs
@@ -1,32 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class DragObject : MonoBehaviour
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class DragObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    void start()
-    {
+    public GameObject Goal;
+    public static Vector2 StartPos;
 
-    }
-    private void Update()
+    public void OnBeginDrag(PointerEventData eventData) 
     {
-
-    }
-    public void Drag_object(string a)
-    {
-        StartCoroutine(MouseDrag());
-
+        StartPos = transform.position; // 드래그 시작할 때 오브젝트 위치 
     }
 
-    IEnumerator MouseDrag()
+    public void OnDrag(PointerEventData eventData)
     {
-        while (Input.GetMouseButtonDown(0))
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if(transform.position == Goal.transform.position) // Goal.transform.position부분 판정때문에 수정해야함
         {
-            Vector2 mouseDragPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            Vector2 worldObjectPositon = Camera.main.ScreenToWorldPoint(mouseDragPosition);
-            this.transform.position = worldObjectPositon;
-            yield return null;
+            if(Goal.transform.tag != "Gun") // 총은 총알 2발 장전하고나서
+            {
+                transform.position = Goal.transform.position;
+                Goal.SetActive(false);
+            }
+            else
+            {
+
+            }
+        }
+        else // 목적지에 맞지 않으면 드래그 시작 지점으로 돌아감
+        {
+            transform.position = StartPos;
         }
     }
+    private void OnTriggerEnter2D(Collider2D target) // collider 충돌 판정. Goal.~ 부분 수정하는 것에 참고? 
+    {
 
+    }
 }
