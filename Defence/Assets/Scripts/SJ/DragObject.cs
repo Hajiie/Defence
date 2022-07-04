316d0626e64bb56b25c490840fd5d084f8d6525c
water-bell <wwkk548@gmail.com>
2022-07-04 21:35:39 +0900

    [Stage3] DragObjcet Fixed

--- Defence/Assets/Scripts/SJ/DragObject.cs
+++ Defence/Assets/Scripts/SJ/DragObject.cs
@@ -7,6 +7,11 @@ public class DragObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
 {
     public GameObject Goal;
     public static Vector2 StartPos;
+    bool isInGoal;
+    int bullNum = 0; // 2가 되면 다른 스크립트에서 단계 넘겨줄 예정..
+    bool step1Check;
+    bool step2Check;
+    bool step3Check; // 장전 단계에 따라 보여질 오브젝트를 다른 스크립트에서 구현할 예정..
 
     public void OnBeginDrag(PointerEventData eventData) 
     {
@@ -20,16 +25,17 @@ public class DragObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        if(transform.position == Goal.transform.position) // Goal.transform.position부분 판정때문에 수정해야함
+        if(isInGoal) 
         {
-            if(Goal.transform.tag != "Gun") // 총은 총알 2발 장전하고나서
+            if(Goal.transform.tag == "GoalArrow") 
             {
-                transform.position = Goal.transform.position;
-                Goal.SetActive(false);
+                transform.position = Goal.transform.position; // 목적지 화살표 위치로 이동
+                Goal.SetActive(false); // 화살표 숨김
             }
-            else
+            if (Goal.transform.tag == "Gun") // 총알 총에 넣는 경우
             {
-
+                this.gameObject.SetActive(false); //총알 넣으면 그 총알 지움
+                bullNum++;
             }
         }
         else // 목적지에 맞지 않으면 드래그 시작 지점으로 돌아감
@@ -37,8 +43,15 @@ public class DragObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
             transform.position = StartPos;
         }
     }
-    private void OnTriggerEnter2D(Collider2D target) // collider 충돌 판정. Goal.~ 부분 수정하는 것에 참고? 
+    private void OnTriggerEnter2D(Collider2D target) // collider 충돌 판정
     {
-
+        if (target.CompareTag("GoalArrow")) 
+        {
+            isInGoal = true;
+        }
+        if (target.CompareTag("Gun"))
+        {
+            isInGoal = true;
+        }
     }
 }
